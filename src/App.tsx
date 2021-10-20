6c30aecea23c48017b93704788ed09d83d978162
lqd1434 <1434288209@qq.com>
2021-10-20 21:09:32 +0800

    feat:增加squareLoading

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,9 @@
 import React from 'react'
-import { CircleLoading1, CircleLoading2, OverLayer, PulseLoading } from './components/CircleLoading'
-import styles from './app.module.scss'
-import useDialog from './components/Dialog'
+import SquareLoading from './components/Square'
 function App() {
-	const { DialogView, openDialog, visible } = useDialog()
-	console.log(visible)
 	return (
 		<>
-			<DialogView />
-			<button onClick={openDialog}>open</button>
+			<SquareLoading />
 		</>
 	)
 }
---
+++ src/components/Square/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './index.module.scss'
+
+const SquareLoading = () => {
+	return (
+		<div className={styles.con}>
+			<div className={styles.squareLoading} />
+		</div>
+	)
+}
+
+export default SquareLoading
